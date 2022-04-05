9ff76cc7f8114437c2d27e3152e7d760c9693407
MastroSo <alessandrogiovannisodano@gmail.com>
2022-04-05 15:03:37 +0200

    Progetto Finito + bonus

--- bookstoreCsharp/Program.cs
+++ bookstoreCsharp/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+//Dichiarazioni
+
+string titolo = "CleanCode";
+string autore = "Robert Martin";
+int ISBN = 12868;
+int numeropagine = 431;
+float pesolibro = 0.66f;
+float lunghezzaLib = 0.40f;
+float larghezzaLib = 0.35f;
+float altezzaLib = 0.30f;
+float valutazionemedia = 4.5f;
+string versionekindle = "Yes";
+string copertinaflessibile = "No";
+
+//ConsoleWriteline
+
+Console.WriteLine("Informazione Generiche" + titolo + "di",autore);
+Console.WriteLine("Informazione Generiche" + ISBN);
+Console.WriteLine("Informazioni Generiche" + numeropagine);
+Console.WriteLine("Informazioni Generiche" + pesolibro);
+Console.WriteLine("Informazioni Dimensioni Libro" + lunghezzaLib);
+Console.WriteLine("Informazioni Dimensioni Libro" + larghezzaLib);
+Console.WriteLine("Informazioni Dimensioni Libro" + altezzaLib);
+Console.WriteLine("Valutazioni" + valutazionemedia);
+Console.WriteLine("Kindle" + versionekindle);
+Console.WriteLine("Copertina" + copertinaflessibile);
